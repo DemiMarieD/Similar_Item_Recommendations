b2a863876b3f26e7400f36815af73a30a40c7c27
DemiMarieD <demi-marie@outlook.de>
2020-05-10 20:48:48 +0200

    - added new information on data import

--- RecommendationApp/operations.py
+++ RecommendationApp/operations.py
@@ -1,25 +1,48 @@
 import pandas as pd
 import tmdbsimple as tmdb
+from pathlib import Path
 from RecommendationApp.strategies import demi
 from RecommendationApp.strategies import eda
 from RecommendationApp.strategies import sebastian
 
 tmdb.API_KEY = '1fe2d017037a1445b9122ea2dcd42d41'
 
-
-movies = pd.read_table("RecommendationApp/data/ml-1m/movies.dat", "::", engine="python", names=['MovieID', 'Title', 'Genres'])
-ratings = pd.read_table("RecommendationApp/data/ml-1m/ratings.dat", "::", engine="python",
+# ***************** DATA IMPORTs ************************
+movies = pd.read_table("data/ml-1m/movies.dat", "::", engine="python",
+                       names=['MovieID', 'Title', 'Genres'])
+ratings = pd.read_table("data/ml-1m/ratings.dat", "::", engine="python",
                         names=['UserID', 'MovieID', 'Rating', 'Timestamp'])
-users = pd.read_table("RecommendationApp/data/ml-1m/users.dat", "::", engine="python",
+users = pd.read_table("data/ml-1m/users.dat", "::", engine="python",
                       names=['UserID', 'Gender', 'Age', 'Occupation', 'Zip-Code'])
-metadata = pd.read_csv(filepath_or_buffer="RecommendationApp/data/movies_metadata.csv", delimiter=",")
 
 # merge all to one -> then make sample and use only this table
 data_right = pd.merge(ratings, movies, on='MovieID')
 data = pd.merge(users, data_right, on='UserID')
 data = data[['UserID', 'MovieID', 'Title', 'Rating', 'Genres']]
-# todo add all other information given (metadata)
-movie_list = list(data["MovieID"].unique())
+
+metadata = pd.read_csv(filepath_or_buffer="data/movies_metadata.csv", delimiter=",")
+
+extracted_content_dict = {}
+pathlist = Path("data/extracted_content_ml-latest/").glob('**/*.json')
+for path in pathlist:
+    path_in_str = str(path)
+    print(path_in_str)
+    f_input = open(path_in_str, 'r', encoding="utf8")
+    content_dict = json.load(f_input)
+    movielensId = content_dict['movielensId']
+    extracted_content_dict[movielensId] = content_dict
+
+
+# tmdb_dict = extracted_content_dict[<movieID>]['tmdb']
+# imdb_dict = extracted_content_dict[<movieID>]['imdb']
+# movielensId = extracted_content_dict[<movieID>]['movielensId']
+# movielens_dict = extracted_content_dict[<movieID>]['movielens']
+# cast_listOfDict = tmdb_dict['credits']['cast']
+# crew_listOfDict = tmdb_dict['credits']['crew']
+# genres_listOfDict = tmdb_dict['genres']
+# ..... for more look at the .json
+
+# ***********************************************************
 
 
 def setup():
