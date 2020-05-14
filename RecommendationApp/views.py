14be670c447d753991a8db091cefb2e7f9265d07
DemiMarieD <demi-marie@outlook.de>
2020-05-14 19:44:20 +0200

    give reverence movie to template

--- RecommendationApp/views.py
+++ RecommendationApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from RecommendationApp.forms import user_id_form
 from RecommendationApp.operations import getTop5s
+from RecommendationApp.operations import getMovieDetails
 from RecommendationApp.operations import setup
 
 def index(request):
@@ -39,6 +40,6 @@ def recommendation_view(request, id):
 
     # make to recommendations_dict, key = method_name, value = dict of movies with their details
     similar_movies_dicts = getTop5s(id)
-    context = {"similar_movies_dicts": similar_movies_dicts.items()}  # todo add reference movies
+    context = {"similar_movies_dicts": similar_movies_dicts.items(), "reference_movie": getMovieDetails([id]).items()}
 
     return render(request, "similar_movies.html", context)
