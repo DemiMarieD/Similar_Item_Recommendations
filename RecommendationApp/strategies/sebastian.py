252509fd519717e7dfc0b0110013a21a6ef7341d
slubos <sebastian.lubos@outlook.com>
2020-05-25 21:26:53 +0200

    Refined methods for contrast and colour histogram

--- RecommendationApp/strategies/sebastian.py
+++ RecommendationApp/strategies/sebastian.py
@@ -1,11 +1,11 @@
 import pickle
 from pathlib import Path
-
 from PIL import Image
 from PIL import ImageStat
 import requests
 from io import BytesIO
-
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 
 def calculate_luminance(r, g, b):
     return (0.299 * r + 0.587 * g + 0.114 * b)
@@ -14,10 +14,17 @@ def calculate_luminance(r, g, b):
 def calculate_average_image_brightness(image):
     try:
         poster_stat = ImageStat.Stat(image)
-        # TODO: Differentiate between grayscale images and coloured ones
-        # Calculate the average RGB values for the image
-        r, g, b = poster_stat.mean
-        avg_brightness = calculate_luminance(r, g, b)
+        image_is_greyscale = False
+        if len(image.getbands()) == 1:
+            # If the image has only one band, it is a greyscale image
+            image_is_greyscale = True
+
+        if image_is_greyscale:
+            avg_brightness = poster_stat.mean()
+        else:
+            # Calculate the average RGB values for the image
+            r, g, b = poster_stat.mean
+            avg_brightness = calculate_luminance(r, g, b)
         return avg_brightness
     except:
         print("Error while trying to calculate the brightness for image.")
@@ -27,15 +34,26 @@ def calculate_average_image_brightness(image):
 def calculate_average_image_contrast(image):
     try:
         poster_stat = ImageStat.Stat(image)
-        # TODO: Differentiate between grayscale images and coloured ones
-        r, g, b = poster_stat.mean
-        avg_intensity = calculate_luminance(r, g, b)
+        image_is_greyscale = False
+        if len(image.getbands()) == 1:
+            # If the image has only one band, it is a greyscale image
+            image_is_greyscale = True
+
+        if image_is_greyscale:
+            avg_intensity = poster_stat.mean()
+        else:
+            r, g, b = poster_stat.mean
+            avg_intensity = calculate_luminance(r, g, b)
+
         sum_contrast = 0
         for x in range(image.width):
             for y in range(image.height):
-                r, g, b = image.getpixel((x, y))
-                pixel_luminance = calculate_luminance(r, g, b)
-                sum_contrast = pixel_luminance - avg_intensity
+                if image_is_greyscale:
+                    pixel_luminance = image.getpixel((x, y))
+                else:
+                    r, g, b = image.getpixel((x, y))
+                    pixel_luminance = calculate_luminance(r, g, b)
+                sum_contrast += pixel_luminance - avg_intensity
 
         avg_contrast = sum_contrast / (image.width * image.height)
         return avg_contrast
@@ -47,11 +65,11 @@ def calculate_average_image_contrast(image):
 def get_image_similarity_histogram(image):
     try:
         histogram = image.histogram()
-        # TODO: Handle grayscale images
         # 256*3 => RGB channels are concatenated
+        # Length is the same for grayscale images
         if len(histogram) != 256*3:
             return None
-        return histogram
+        return np.asarray(histogram)
     except:
         print("Error while trying to get colour histogram for image.")
         return None
@@ -62,12 +80,10 @@ def get_image_similarity_histogram(image):
 def calculate_image_similarity(value_image_1, value_image_2):
     return 1 - abs(value_image_1 - value_image_2)
 
-# The similarity method simple calculates the deviation of two histograms
+# The similarity method uses the cosine similarity between tow vectors
 def calculate_image_similarity_histogram(histogram_1, histogram_2):
-    # Subtract the values pairwise and take the absolute values
-    subtracted_difference = [abs(x1 - x2) for (x1, x2) in zip(histogram_1, histogram_2)]
-    # Sum up the differences
-    return sum(subtracted_difference)
+    similarity = cosine_similarity([histogram_1], [histogram_2])
+    return similarity
 
 # Returns the image as Pillow image if available, otherwise None
 def get_image_from_url(url):
@@ -165,7 +181,7 @@ class Image_Based_Recommender:
                 relevant_movies.append(
                     (m_id, calculate_image_similarity_histogram(target_movie_histogram, value['colour_histogram'])))
 
-        result = get_top_5(relevant_movies, False)
+        result = get_top_5(relevant_movies, True)
         return result
 
     # Loads the serialized movieposter_metadata object
